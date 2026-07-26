633fbe2c19f77775a58f33277cf2adff904bac6d
Ali Ahsan <aliahsan8751@gmail.com>
2026-07-26 17:58:10 +0500

    added vector Db

--- BusinessOS.Application/Features/VectorSearch/Options/QdrantOptions.cs
+++ BusinessOS.Application/Features/VectorSearch/Options/QdrantOptions.cs
@@ -6,7 +6,7 @@ public sealed class QdrantOptions
 
     public bool Enabled { get; set; } = true;
     public string Host { get; set; } = "localhost";
-    public int Port { get; set; } = 6333;
+    public int Port { get; set; } = 6334;
     public bool Https { get; set; }
     public string ApiKey { get; set; } = string.Empty;
     public string CollectionName { get; set; } = "businessos_knowledge";
